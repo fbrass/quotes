cb35190b6c289f35bb97afe7b9fb52a599c922d7
Marcus Geiger <web0c@antbear.org>
2013-02-09 18:12:54 +0100

    Updated validation message for passwords, updated db schema to contain delete cascade specs.

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -9,26 +9,32 @@ create table users (
 
 create table authorities (
   email     varchar_ignorecase(128) not null,
-  authority varchar_ignorecase(50)  not null,
-  constraint fk_authorities_users foreign key (email) references users (email)
+  authority varchar_ignorecase(50)  not null
 );
+
+alter table authorities add foreign key (email) references users (email)
+  on delete cascade;
 
 create unique index idx_authorities_email on authorities (email, authority);
 
 create table persistent_logins (
-  email  varchar(128) not null,
-  series varchar(64)  primary key,
-  token  varchar(64)  not null,
+  username  varchar(128) not null,
+  series    varchar(64)  primary key,
+  token     varchar(64)  not null,
   last_used timestamp not null
 );
 
+alter table persistent_logins add foreign key (username) references users (email)
+  on delete cascade;
+
 create table todolist (
-  id bigint not null primary key auto_increment,
-  user varchar_ignorecase(50) not null,
+  id       bigint not null primary key auto_increment,
+  user     varchar_ignorecase(50) not null,
   listname varchar(64) not null,
-  created timestamp not null default now(),
-  constraint fk_todolist_users foreign key (user) references users (email)
+  created  timestamp not null default now()
 );
+
+alter table todolist add foreign key (user) references users (email);
 
 create unique index idx_todolist_user_listname on todolist (user, listname);
 
