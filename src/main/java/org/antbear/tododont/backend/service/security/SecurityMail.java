2827b887120a0149d8a5e74858fce6de1c07706d
Marcus Geiger <web0c@antbear.org>
2013-02-18 18:21:20 +0100

    Removed unnecessary TODO.

--- src/main/java/org/antbear/tododont/backend/service/security/SecurityMail.java
+++ src/main/java/org/antbear/tododont/backend/service/security/SecurityMail.java
@@ -2,11 +2,6 @@ package org.antbear.tododont.backend.service.security;
 
 import org.springframework.mail.SimpleMailMessage;
 
-/**
- * TODO add docs
- *
- * @author marcus
- */
 public interface SecurityMail {
 
     String getEmail();
