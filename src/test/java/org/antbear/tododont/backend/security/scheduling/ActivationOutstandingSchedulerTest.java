9b62a3c66d9a6477748cd7e77cc467c3dc95822c
Marcus Geiger <web0c@antbear.org>
2013-02-25 18:47:50 +0100

    Added more tests to ActivationOutstandingSchedulerTest.

--- src/test/java/org/antbear/tododont/backend/security/scheduling/ActivationOutstandingSchedulerTest.java
+++ src/test/java/org/antbear/tododont/backend/security/scheduling/ActivationOutstandingSchedulerTest.java
@@ -5,6 +5,7 @@ import org.antbear.tododont.backend.security.entity.CustomUserDetails;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -12,7 +13,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:/test-context.xml")
@@ -39,5 +43,19 @@ public class ActivationOutstandingSchedulerTest {
 
         // Throws UsernameNotFound because user has been deleted
         this.userDetailsService.loadUserByUsername(user.getUsername());
+    }
+
+    @Test
+    public void testOnScheduleDoNotDeleteTest() throws Exception {
+        CustomUserDetails user = new CustomUserDetails("ignored8888@nowhere.tld", "ignored-token");
+        assertFalse(user.isEnabled());
+        user.setPassword("secretPassword@IGNORED");
+        this.userDetailsService.createUser(user);
+
+        activationOutstandingScheduler.onSchedule();
+
+        final UserDetails userDetails = this.userDetailsService.loadUserByUsername(user.getUsername());
+        assertNotNull(userDetails);
+        assertThat(userDetails.getUsername(), is(user.getUsername()));
     }
 }
